eb07a8de2c221c087cf0d2b7958a25c864193da3
Mikael Stefánsson <54936483+penguinator9000@users.noreply.github.com>
2022-11-13 12:27:02 +0000

    loads the stuff

--- Control3DBase/levels/levelloader.py
+++ Control3DBase/levels/levelloader.py
@@ -1,29 +1,52 @@
 import csv
 import json
-
+import sys
 class LevelLoader():
     def __init__(self,location):
         self.root = location
+        self.levelDir = ""
     def load(self,levelDir):
-        if self.levelDir != levelDir:
-            self.levelDir = levelDir
-        else: return "already loaded"
-        layoutFile = open(leveldir+"/layout.csv",'r')
-
-        with open(sys.path[0] + "/maze.csv", 'r') as file:
-            csvreader = csv.reader(file)
+        if self.levelDir == levelDir:
+            return "already loaded"
+        
+        self.levelDir = levelDir
+        print(levelDir+"/layout.csv")
+        with open(self.root+"/"+levelDir+"/layout.csv",'r') as layoutFile:
+            csvreader = csv.reader(layoutFile)
             first = True
+            ycount = 0
+            zcount = 0
+            
             for row in csvreader:
                 if first: 
                     first =False
                     self.zMax,self.xMax,self.yMax = row
+                    self.zMax = int(self.zMax)
+                    self.xMax = int(self.xMax)
+                    self.yMax = int(self.yMax)
+                    self.layout = [[[]for j in range(self.zMax)] for i in range(self.yMax)]
                 else:
-                    x,z = [(int(val)) for val in row]
-                    temp = GraphicalObject(c,pos=(x*2+MAZE_ofset,1,z*2+MAZE_ofset),size=(2,3,2),color=Color((x)/MAZE_Max,1-min(1,((x)/MAZE_Max+(z)/MAZE_Max)),(z)/MAZE_Max))
-                    self.mazeObjects.append(temp)
-                    self.maze[x][z]=temp    
+                    wall = True
+                    if self.zMax == zcount:
+                        zcount = 0
+                        ycount += 1
+                    for i in  row:
+                        for j in range(int(i)):
+                            self.layout[ycount][zcount].append(wall) #exchange this for object at some point.
+                        wall = not wall
+                    zcount += 1
+        with open(self.root+"/"+levelDir+"/objects.json",'r') as objects:
+            self.objects = json.load(objects)
+        return [self.layout, self.objects]
+
+                       
 
 
     def changeRoot(self,location):
         self.root = location
         #maybe clear data later. also function might be useless
+
+
+if __name__ == "__main__":
+    ll = LevelLoader(sys.path[0])
+    print(ll.load("buttons"))
